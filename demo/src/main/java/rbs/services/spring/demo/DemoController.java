2c24943b7035111b29d258f6e2f580e26a48329e
AvikS <avik.sengupta@royalmail.com>
2018-11-27 07:16:57 +0000

    coin link added

--- demo/src/main/java/rbs/services/spring/demo/DemoController.java
+++ demo/src/main/java/rbs/services/spring/demo/DemoController.java
@@ -1,13 +1,31 @@
 package rbs.services.spring.demo;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DemoController {
 
-    @RequestMapping("/")
-    public String index(){
-        return "{'hello': 'world'}";
+    @GetMapping("weather/{city}")
+    //public Response Weather(@RequestParam(value="name") String name) {
+    public Response Weather(@PathVariable String city) {
+        
+        return new Response(city);
+    }
+
+    @GetMapping("cryptos/")
+    public Coin[] GetCoins(@RequestParam(value="name", defaultValue = "") String name)  {
+        
+        RestTemplate restTemplate = new RestTemplate();
+
+        Coin[] coins = restTemplate.getForObject("https://api.coinmarketcap.com/v1/ticker/" + name, Coin[].class);
+        return coins;
     }
 }
---
+++ demo/src/main/java/rbs/services/spring/demo/Response.java
@@ -0,0 +1,18 @@
+package rbs.services.spring.demo;
+
+
+
+public class Response{
+    public Response(String name){
+        this.Id =1;
+        this.Name = name;
+        
+    }
+
+    public int Id;
+    public String Name;
+}
+
+
+
+
